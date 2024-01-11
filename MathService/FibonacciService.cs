189d4f08279814af8644894ce5b2839615102498
Megan237 <megan.stinefield@bsu.edu>
2024-01-11 13:18:42 -0500

    initial files

---
+++ MathService/FibonacciService.cs
@@ -0,0 +1,13 @@
+﻿namespace MathService;
+
+public class FibonacciService
+{
+    public static int GetFibValueOf(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return n;
+        }
+        return GetFibValueOf(n - 1) + GetFibValueOf(n - 2);
+    }
+}
